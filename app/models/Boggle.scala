91fedb9fa98249b62e9bfa6348c0b570b5c36d36
Austin <jeffreyaustinlong@gmail.com>
2022-03-02 20:13:30 -0500

    getting closer

--- app/models/Boggle.scala
+++ app/models/Boggle.scala
@@ -1,5 +1,52 @@
 package models
 
+import scala.collection.IterableOnce.iterableOnceExtensionMethods
+
 case class Board()
 
-case class Spot(position: Int, connectedPositions: Seq[Int])
+case class Spot(char: Char, position: Int, connectedPositions: Seq[Int])
+
+case class Letter(leter: Char, weight: Int)
+
+object WeightedLeters {
+
+  val leters = Map[Char, Int](
+    ('A' -> 80),
+    ('B' -> 16),
+    ('C' -> 30),
+    ('D' -> 44),
+    ('E' -> 120),
+    ('F' -> 25),
+    ('G' -> 17),
+    ('H' -> 64),
+    ('I' -> 80),
+    ('J' -> 4),
+    ('K' -> 8),
+    ('L' -> 40),
+    ('M' -> 30),
+    ('N' -> 80),
+    ('O' -> 80),
+    ('P' -> 117),
+    ('Q' -> 5),
+    ('R' -> 62),
+    ('S' -> 80),
+    ('T' -> 90),
+    ('U' -> 34),
+    ('V' -> 12),
+    ('W' -> 20),
+    ('X' -> 4),
+    ('Y' -> 20),
+    ('Z' -> 2))
+
+  var totalWeight = leters.map(_._2).sum
+
+  def getRandomWeightedLetter(): Char = {
+    // I'm sure this can be done in a more scalaish way
+    var randomInt = (Math.random() * totalWeight).toInt - 1
+    leters.takeWhile(leter => {
+      randomInt = randomInt - leter._2
+      randomInt > 0
+    }).head._1
+  }
+
+}
--- app/services/BogglePlayer.scala
+++ app/services/BogglePlayer.scala
@@ -1,49 +1,51 @@
 package services
 
 import akka.stream.Materializer
-import models.Spot
+import models.{Spot, WeightedLeters}
 
 import javax.inject.Inject
-import scala.+:
-import scala.concurrent.Future
-import scala.concurrent.ExecutionContext.Implicits.global
 
 class BogglePlayer @Inject()(implicit val materializer: Materializer) {
 
-  def playBoggle = {
+  def playBoggle(size: Int) = {
 
-    // define board
+    val board = generateBoard(size)
 
-    //
+    def getSpot(num: Int) = board.filter(_.position == num).head
+
 
   }
 
-  def generateBoard(size: Int): Future[Seq[Spot]] = {
+  def searchFromHere(spot: Spot)
 
-    // given a squre of size x size, how do I make a sequence of spots
-    // that know what its connected spots are?
+  private def search(board: Seq[Spot]): Seq[String] = {
+
+    var solutions
+
+  }
+
+  private def generateBoard(size: Int): Seq[Spot] = {
 
     var spots: Seq[Spot] = Seq[Spot]()
 
     for (row <- 0 until size - 1) {
       for (column <- 0 until size - 1) {
         val pos = row*column
-        val spot = Spot(pos, getConnectedposs(pos, size))
+        val spot = Spot(WeightedLeters.getRandomWeightedLetter(), pos, getConnectedPoss(pos, size))
         spots = spot +: spots
       }
     }
-
-    Future.successful(spots)
+    spots
   }
 
-  def getConnectedposs(pos: Int, size: Int): Seq[Int] = {
+  private def getConnectedPoss(pos: Int, size: Int): Seq[Int] = {
 
       (pos, size) match {
         case (_, size) if size < 3 => throw new Exception("Board is less than 3x3. Not Bogglable!")
         case (0, _) => Seq[Int](1, size, size + 1) // top Left
         case (pos, size) if pos == size -1 => Seq[Int](size -2, size*2-2, size*2-1) // top Right
         case (pos, size) if pos == size * size - size => Seq[Int](getSpotAbove(pos, size), getSpotAboveRight(pos, size), getSpotRight(pos)) // bottom left
-        case (pos, size) if pos == size - 1 => Seq[Int](getSpotAbove(pos, size), getSpotLeft(pos), getSpotAboveLeft(pos, size)) // botom right
+        case (pos, size) if pos == size - 1 => Seq[Int](getSpotAbove(pos, size), getSpotLeft(pos), getSpotAboveLeft(pos, size)) // bottom right
         case (pos, size) if pos < size => Seq[Int](getSpotLeft(pos), getSpotRight(pos), getSpotBelow(pos, size), getSpotBelowRight(pos, size), getSpotBelowLeft(pos, size)) // top row
         case (pos, size) if pos > size * size - size => Seq[Int](getSpotLeft(pos), getSpotRight(pos), getSpotAbove(pos, size), getSpotAboveRight(pos, size), getSpotAboveLeft(pos, size)) // top row
         case (pos, size) if pos % size == 0 => Seq[Int](getSpotAbove(pos, size), getSpotAboveRight(pos, size), getSpotRight(pos), getSpotBelowLeft(pos, size), getSpotBelow(pos, size)) // left side
