ecc57cf4c3a3b236a0c308b36d080c23a69451de
Austin <jeffreyaustinlong@gmail.com>
2022-03-02 21:03:41 -0500

    closer, but doesn't feel right

--- app/services/BogglePlayer.scala
+++ app/services/BogglePlayer.scala
@@ -7,21 +7,37 @@ import javax.inject.Inject
 
 class BogglePlayer @Inject()(implicit val materializer: Materializer) {
 
+
+
   def playBoggle(size: Int) = {
 
     val board = generateBoard(size)
-
-    def getSpot(num: Int) = board.filter(_.position == num).head
-
-
+    search(board, Seq[Spot](board.head), Seq[String]())
   }
 
-  def searchFromHere(spot: Spot)
+  private def search(board: Seq[Spot], currentSpots: Seq[Spot], results: Seq[String]): Seq[String] = {
 
-  private def search(board: Seq[Spot]): Seq[String] = {
+    val currentWord: String = currentSpots.map(_.char).foldLeft[String]("")((soFar, char) => soFar.appended(char))
+    val unusedAdjacentSpots: Seq[Spot] = currentSpots
+      .last
+      .connectedPositions
+      .map(position => board.filter(_.position == position).head)
+      .filterNot(spot => currentSpots.contains(spot))
 
-    var solutions
-
+    if (DictionarySearcher.isWord(currentWord)) {
+      if (unusedAdjacentSpots.size > 0) {
+        unusedAdjacentSpots.map(spot => search(board, currentSpots :+ spot, currentWord +: results)).flatten
+      }
+      else {
+        currentWord +: results
+      }
+    }
+    else if (DictionarySearcher.wordsExistsThatStartWith(currentWord)) {
+      unusedAdjacentSpots.map(spot => search(board, currentSpots :+ spot, currentWord +: results)).flatten
+    }
+    else {
+      results
+    }
   }
 
   private def generateBoard(size: Int): Seq[Spot] = {
