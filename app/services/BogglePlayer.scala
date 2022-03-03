96531ca81b9cc62242363178fd1089cd71e0dd51
Austin <jeffreyaustinlong@gmail.com>
2022-03-03 09:20:51 -0500

    comments!

--- app/services/BogglePlayer.scala
+++ app/services/BogglePlayer.scala
@@ -10,10 +10,35 @@ class BogglePlayer @Inject()(implicit val materializer: Materializer) {
   def playBoggle(size: Int) = {
 
     val board = generateBoard(size)
-    search(board, Seq[Spot](board.head))
+    // call the search method on every possible starting spot
+    // That will be every spot on the board
+    // .flatten.distinct will reduce the list to a single flat list of strings and remove any duplicates
+    board.map(spot => search(board, Seq[Spot](spot)))
+      .flatten
+      .distinct
   }
 
-  private def search(board: Seq[Spot], currentSpots: Seq[Spot]): Seq[String] = {
+  /**
+   * This is the recursive method that performs the meat of the logic.
+   * It takes a board and a sequence of spots on the board that have been visited.
+   * CurrentSpots will start as a single spot indicating a start point.
+   * It covers three possible situations:
+   * 1) the current sequence of spots creates a word.
+   *      In this case we return the new word + the return value of the the progression of the recursive method
+   * 2) the current sequence of spots can match the start of a possible future word
+   *      In this case we simply continue the recursive method and return its result
+   * 3) the current sequence of spots is not a word nor matches the start of a possible future word
+   *      This means there's no reason to continue the recursive method and we should return an empty list.
+   * In cases 1 and 2, if there are no more adjacent spots that have not been visited,
+   * the recursive method will not continue
+   *
+   * The result should be a sequence of strings that can be generated from the initial spot.
+   *
+   * @param board
+   * @param currentSpots
+   * @return
+   */
+  private def search(board: Seq[Spot], currentSpots: Seq[Spot] = Seq[Spot]()): Seq[String] = {
 
     val currentWord: String = currentSpots.map(_.char).foldLeft[String]("")((soFar, char) => soFar.appended(char))
     val unusedAdjacentSpots: Seq[Spot] = currentSpots
